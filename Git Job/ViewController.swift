f3c963d02c3532de3a4ad39b2bfaf505990f5a32
JosiahRininger <josiahrininger@gmail.com>
2020-03-22 20:52:16 -0500

    Sets up project, SwiftLint file, Swift Flags, and .gitignore

---
+++ Git Job/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Job
+//
+//  Created by Josiah Rininger on 3/22/20.
+//  Copyright © 2020 RinDanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
